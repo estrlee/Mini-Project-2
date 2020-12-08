837ea5b8fd3adf37c60d29b55016ffcaf1bdd883
estrlee <65265093+estrlee@users.noreply.github.com>
2020-12-07 21:32:03 -0500

    Update MiniProject2.c

--- MiniProject2.c
+++ MiniProject2.c
@@ -1,8 +1,6 @@
 /******************************************************************************
 
 Esther Lee 
-Austin Marshburn
-Elijah Tipton
 ELET 1102
 Project 2: Iteration - Discount Electronics
 This code displays the price of a product based on its item number, unit price, and 
