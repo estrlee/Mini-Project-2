79cd4ebf0d54ca59cae557da43df1b60d4753f66
Austin Marshburn <73564104+Amarshbu@users.noreply.github.com>
2020-12-11 01:18:10 -0500

    Made into table form

--- MiniProject2.c
+++ MiniProject2.c
@@ -1,21 +1,19 @@
 /******************************************************************************
-
 Esther Lee, Elijah Tipton, Austin Marshburn
 ELET 1102
 Project 2: Iteration - Discount Electronics
 This code displays the price of a product based on its item number, unit price, and 
 quantity.
-
 *******************************************************************************/
 #include <stdio.h>
 
 int main()
 {
-    printf("Item Number    Description    Discount 0-299   Discount 300-499   Discount 500+\n");
-    printf("     0          Resistor            5%%            7.5%%              10%%\n");
-    printf("     1          Capacitor           5%%            7.5%%              10%%\n");
-    printf("     2          Transistor         2.5%%            5%%               10%%\n");
-    printf("     3      Integrated Circuits    2.5%%            5%%               10%%\n");
+    printf("| Item Number |     Description     | Discount 0-299 | Discount 300-499 | Discount 500+ |\n");
+    printf("|      0      |      Resistor       |       5%%       |       7.5%%       |       10%%     |\n");
+    printf("|      1      |      Capacitor      |       5%%       |       7.5%%       |       10%%     |\n");
+    printf("|      2      |     Transistor      |      2.5%%      |        5%%        |       10%%     |\n");
+    printf("|      3      | Integrated Circuits |      2.5%%      |        5%%        |       10%%     |\n");
     
     int quantity;
     int itemNum;
@@ -24,7 +22,7 @@ int main()
     double ogPrice;
     double discount;
     
-    printf("Enter the quantity, item number, and unit cost: ");
+    printf("\nEnter the quantity, item number, and unit cost: ");
     scanf("%d %d %lf", &quantity, &itemNum, &unitCost);
     
     if(itemNum == 0 || itemNum == 1){
